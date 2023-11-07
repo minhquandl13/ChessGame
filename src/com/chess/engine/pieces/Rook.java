3d711824b45d4fd5f7353c1e34691c041a141b93
minhquandl13 <64225858+minhquandl13@users.noreply.github.com>
2023-11-07 23:58:04 +0700

    update algorithm for rook and queen movement

---
+++ src/com/chess/engine/pieces/Rook.java
@@ -0,0 +1,64 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.*;
+
+public class Rook extends Piece {
+    // These are offsets with respect to our current position
+    private final static int[] CANDIDATE_MOVE_COORDINATE_VECTOR = {-8, -1, 1, 8};
+
+    public Rook(final int piecePosition, final Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int candidateCoordinateOffset : CANDIDATE_MOVE_COORDINATE_VECTOR) {
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while (BoardUtils.isValidCoordinate(candidateDestinationCoordinate)) {
+                if (isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)
+                        || isEighthColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)) {
+                    break;
+                }
+
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+
+                if (BoardUtils.isValidCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if (candidateDestinationTile.isTileOccupied()) { // not occupied then add sort of a non-atacking legal move
+                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                        if (this.pieceAlliance != pieceAlliance) { // enemy piece
+                            legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition]
+                && (candidateOffset == -1);
+    }
+
+    private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGHT_COLUMN[currentPosition]
+                && (candidateOffset == 1);
+    }
+}
